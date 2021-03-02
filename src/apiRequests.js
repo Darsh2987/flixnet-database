1d68ef5943c9d2b9ae68d0db37f8636fd1695472
Darsh <darsh2987@gmail.com>
2021-03-02 12:51:11 +0000

    renamed request file

--- src/apiRequests.js
+++ src/apiRequests.js
@@ -6,6 +6,7 @@ const baseURL = "https://api.themoviedb.org/3";
 
 // Object containing all API URL endpoints
 const apiRequests = {
+  flixnetOriginals: `${baseURL}/discover/tv?api_key=${apiKey}&with_networks=213`,
   trending: `${baseURL}/trending/all/week?api_key=${apiKey}&language=en-US`,
   popularMovies: `${baseURL}/movie/popular?api_key=${apiKey}&language=en-US&page=1`,
   upcomingMovies: `${baseURL}/movie/upcoming?api_key=${apiKey}&language=en-US&page=1`,
