072008877ac0000f6ab7a6aa8c3cbd80271dc0aa
Darsh <darsh2987@gmail.com>
2021-03-02 12:56:52 +0000

    added content to grid items

--- src/components/DisplayGrid.js
+++ src/components/DisplayGrid.js
@@ -1,13 +1,16 @@
 import React from "react";
 
 const DisplayGrid = (props) => {
+  const imgBaseUrl = "https://image.tmdb.org/t/p/w1280";
   return (
-    <div>
+    <div className="grid">
       {props.movie.map((item) => (
-        <div>
-          <h1>{item.title || item.name}</h1>
-          <p>{item.overview}</p>
-          <img src={"https://image.tmdb.org/t/p/w1280" + item.poster_path} style={{ width: "200px" }} alt={item.title} />
+        <div className="grid__item" key={item.id}>
+          <h1 className="grid__item-title">{item.title || item.name}</h1>
+          <p className="grid__item-overview">{item.overview}</p>
+          <p className="grid__item-vote">{item.vote_average}</p>
+          <p className="grid__item-releasedate">{item.release_date}</p>
+          <img className="grid__item-image" src={imgBaseUrl + item.poster_path} style={{ width: "200px" }} alt={item.title || item.name} />
         </div>
       ))}
     </div>
