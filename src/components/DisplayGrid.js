b66236caa2112eecbaf043432eb316f64693c533
Darsh <darsh2987@gmail.com>
2021-03-05 14:13:00 +0000

    refine code for no image fallback

--- src/components/DisplayGrid.js
+++ src/components/DisplayGrid.js
@@ -1,6 +1,9 @@
 import React from "react";
+import noImage from "../no-image.png";
 
 const DisplayGrid = (props) => {
+  const imageBaseUrl = "https://image.tmdb.org/t/p/original";
+
   return (
     <div className="grid">
       {props.movie.map((item) => (
@@ -8,14 +11,14 @@ const DisplayGrid = (props) => {
           className="grid__item"
           key={item.id}
           style={{
-            backgroundImage: `url(${"https://image.tmdb.org/t/p/original" + item.poster_path})`,
+            backgroundImage: `url(${imageBaseUrl + item?.poster_path ? imageBaseUrl + item?.poster_path : noImage})`,
           }}
         >
           <div className="grid__item-inner">
             <div className="grid__item-description">
-              <h1 className="grid__item-title">{item.title || item.name}</h1>
-              <p className="grid__item-overview">{item.overview}</p>
-              <p className="grid__item-vote">{item.vote_average}</p>
+              <h1 className="grid__item-title">{item?.title || item?.name}</h1>
+              <p className="grid__item-overview">{item?.overview}</p>
+              <p className="grid__item-vote">{item?.vote_average}</p>
             </div>
           </div>
         </div>
